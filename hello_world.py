a20dd154c3a0376aaf071dc76d36b5440f95d280
Vilantino Fernandion <vilantino11@gmail.com>
2020-06-14 00:34:11 +0700

    menambah fitur jumlah total harga

--- hello_world.py
+++ hello_world.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data_barang.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 db = SQLAlchemy(app)
 
 class Barang(db.Model):
@@ -11,12 +11,16 @@ class Barang(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nama_barang = db.Column(db.String(100), nullable=False)
     jumlah_barang = db.Column(db.Integer, nullable=False, default=1)
-    harga_barang = db.Column(db.Integer, nullable=False)
-    jumlah_total = db.Column(db.Integer, nullable=False)
+    harga_beli_barang = db.Column(db.Integer, nullable=False)
+    harga_jual_barang = db.Column(db.Integer, nullable=False)
+    diskon = db.Column(db.Integer,nullable=False)
     tanggal_pembuatan = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return 'ID Barang' + str(self.id)
+        return 'Barang ' + str(self.nama_barang)
+
+db.create_all()
+db.session.commit()
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -24,15 +28,20 @@ def index():
     if request.method == 'POST':
         input_barang = request.form['nama_barang']
         input_jumlah = request.form['jumlah_barang']
-        input_harga = request.form['harga_barang']
-        jml_total = int(input_harga)*int(input_jumlah)
-        post = Barang(nama_barang=input_barang, jumlah_barang=input_jumlah, harga_barang=input_harga, jumlah_total=jml_total)
+        input_harga_beli = request.form['harga_beli_barang']
+        input_harga_jual = request.form['harga_jual_barang']
+        input_diskon = request.form['diskon']
+        
+        post = Barang(nama_barang=input_barang, jumlah_barang=input_jumlah, harga_beli_barang=input_harga_beli, harga_jual_barang=input_harga_jual, diskon=input_diskon)
         db.session.add(post)
         db.session.commit()
         return redirect('/')
     else:
-        posts = Barang.query.(Barang.tanggal_pembuatan).all()
-        return render_template('index.html', posts=posts)
+        posts = Barang.query.order_by(Barang.tanggal_pembuatan).all()
+        for i in posts:
+            jml_total = i.harga_beli_barang * i.jumlah_barang
+        print(jml_total)
+        return render_template('index.html', posts=posts, jml_total=jml_total)
 
 @app.route('/delete/<int:id>')
 def delete(id):
@@ -49,7 +58,9 @@ def update(id):
     if request.method == 'POST':
         post.nama_barang = request.form['nama_barang']
         post.jumlah_barang = request.form['jumlah_barang']
-        post.harga_barang = request.form['harga_barang']
+        post.harga_beli_barang = request.form['harga_beli_barang']
+        post.harga_jual_barang = request.form['harga_jual_barang']
+        post.diskon = request.form['diskon']
         db.session.commit()
         return redirect('/')
     else:
